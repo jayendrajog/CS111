30fc7d39a0e443fecd40228c5904b8f351cb067a
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-02-15 21:43:52 -0800

    Now passes test cases 1-16

--- lab2/osprd.c
+++ lab2/osprd.c
@@ -36,7 +36,7 @@
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("CS 111 RAM Disk");
 // EXERCISE: Pass your names into the kernel as the module's authors.
-MODULE_AUTHOR("Skeletor");
+MODULE_AUTHOR("Jay and Anthony");
 
 #define OSPRD_MAJOR	222
 
