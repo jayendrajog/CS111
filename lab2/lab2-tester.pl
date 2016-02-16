b42b082ae55b68ef0493c56dfdce0025afa7a348
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-02-15 22:14:23 -0800

    Merge test cases

--- lab2/lab2-tester.pl
+++ lab2/lab2-tester.pl
@@ -166,6 +166,12 @@ close FOO;
       ') 2>/dev/null',
       "aX"
     ],
+
+	#18
+	[
+	'(echo foo | ./osprdaccess -w -l /dev/osprda /dev/osprda)',
+	"ioctl OSPRDIOACQUIRE: deadlock avoided"
+	]
     );
 
 my($ntest) = 0;
